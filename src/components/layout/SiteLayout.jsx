531fa655ccc92e4a9e3fa5fc38e62a1af505b3eb
ODUZZ <tobiloba428@gmail.com>
2026-03-20 10:29:56 +0100

    fix: reset scroll on route change

--- src/components/layout/SiteLayout.jsx
+++ src/components/layout/SiteLayout.jsx
@@ -4,10 +4,12 @@ import Navbar from "./Navbar.jsx";
 import Footer from "./Footer.jsx";
 import FloatingContactCTA from "./FloatingContactCTA.jsx";
 import OduzzAssistantWidget from "./OduzzAssistantWidget.jsx";
+import ScrollToTop from "./ScrollToTop.jsx";
 
 export default function SiteLayout() {
   return (
     <>
+      <ScrollToTop />
       <a className="skip" href="#content">Skip to content</a>
       <Navbar />
       <main id="content">
